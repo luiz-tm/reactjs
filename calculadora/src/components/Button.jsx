c6fcae62fef65c6a68b402f4d9c76df3b99f4b2b
Luiz Henrique <58668840+luiz-tm@users.noreply.github.com>
2022-09-23 16:48:37 -0300

    iniciado projeto calculadora

---
+++ calculadora/src/components/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Button.css'
+
+export default function Button(props) {
+    
+    let classes = 'button '
+    classes  += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
+    return (
+        <button className={classes}>
+            {props.label}
+        </button>
+    )
+}
